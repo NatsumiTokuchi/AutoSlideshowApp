bc0399829f6c0352ffbb6f8ef5a5ab61ff9badfd
NatsumiTokuchi <elizabeth.nt77@gmail.com>
2020-07-27 21:06:59 +0900

    【提出用】進む・戻るボタン、再生ボタンの中身を記述。

--- app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
+++ app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
@@ -8,21 +8,22 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
-import android.os.Handler
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     // 許可のための変数
-    private val  PERMISSIONS_REQUEST_CODE = 100
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     // タイマーのための変数
     private var mTimer: Timer? = null
 
     private var mTimerSec = 0.0
 
-    private  var mHandler = Handler()
+    private var mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,26 +35,14 @@ class MainActivity : AppCompatActivity() {
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 getContentsInfo()
             } else {
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
             }
             // Android 5系以下の場合
         } else {
             getContentsInfo()
-        }
-
-        // タイマーを設定する
-        play_pause_button.setOnClickListener {
-            if (mTimer == null) {
-                mTimer = Timer()
-                mTimer!!.schedule(object: TimerTask() {
-                    override fun run() {
-                        mTimerSec += 2.0
-                        mHandler.post {
-
-                        }
-                    }
-                }, 2000, 2000)
-            }
         }
     }
 
@@ -67,7 +56,8 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContentsInfo()
                 } else {
-                    Toast.makeText(applicationContext, "アプリを利用するには許可が必要です", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, "アプリを利用するには許可が必要です", Toast.LENGTH_LONG)
+                        .show()
                 }
         }
     }
@@ -81,17 +71,94 @@ class MainActivity : AppCompatActivity() {
             null,
             null
         )
-
         if (cursor!!.moveToFirst()) {
-            do {
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                Log.d("ASA", "URI : $imageUri")
-
-            } while (cursor.moveToNext())
+            imageView.setImageURI(imageUri)
+            Log.d("ASA", "URI : $imageUri")
         }
-        cursor.close()
+            forward_button.setOnClickListener {
+                if (mTimer == null) {
+                    getContentInfoForward(cursor)
+                }
+            }
+
+            back_button.setOnClickListener {
+                if (mTimer == null) {
+                    getContentInfoBack(cursor)
+                }
+            }
+
+        play_pause_button.setOnClickListener {
+            if (mTimer == null) {
+                play_pause_button.text = "停止"
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mTimerSec += 2.0
+                        mHandler.post {
+                            getContentInfoForward(cursor)
+                        }
+                    }
+                }, 2000, 2000)
+            } else {
+                mTimer!!.cancel()
+                mTimer = null
+                play_pause_button.text = "再生"
+            }
+        }
+    }
+
+    private fun getContentInfoForward(cursor: Cursor) {
+        if (cursor.isLast) {
+            cursor.moveToFirst()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+
+            Log.d("ASA", "URI : $imageUri")
+        } else {
+            cursor.moveToNext()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+
+            Log.d("ASA", "URI : $imageUri")
+        }
+    }
+
+
+    private fun getContentInfoBack(cursor: Cursor) {
+        if (cursor.isFirst){
+            cursor.moveToLast()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            Log.d("ASA", "URI : $imageUri")
+
+            imageView.setImageURI(imageUri)
+        } else {
+            cursor.moveToPrevious()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            Log.d("ASA", "URI : $imageUri")
+
+            imageView.setImageURI(imageUri)
+    }
     }
 }
+
